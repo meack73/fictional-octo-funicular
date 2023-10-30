9b3a9020c4f74802edfaeaa2c8d5a35d17812eae
njneered <n.lacambra@ufl.edu>
2023-10-30 18:39:36 -0400

    Decode Func

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello Meaghan")
+# encode - 
+
+
+
+
+return result
+# decode - NJ
+def decoder(message):
+	result = ""
+	for number in message:
+		new_number = str((int(digit) - 3) % 10)
+		result += new_number
+	return result
 
