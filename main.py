5b1ebfc43be759711d68fe8728f9ae45fb7eff13
Meaghan Knowles <knowles.mc@ufl.edu>
2023-10-30 19:13:06 -0400

    testing to see if correct, changed digit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def menu_display():
 def decoder(message):
 	result = ""
 	for number in message:
-		new_number = str((int(digit) - 3) % 10)
+		new_number = str((int(number) - 3) % 10)
 		result += new_number
 	return result
 
